faac2a2af8ea793c8b711484c7f464089a8246eb
BarkleyRhoat <barkleyrhoat@gmail.com>
2023-11-04 01:52:31 -0400

    added more things to show from the api

--- main/assets/scritp.js
+++ main/assets/scritp.js
@@ -3,10 +3,9 @@ console.log("script", "connected");
 var apiKey = "4e5568a0982d91762ed501f8faa3eb5c";
 
 function fetchWeatherData(city) {
-    // var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-    // var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&units=imperial&appid=${apiKey}`;
-    // var weatherUrl = 'http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}'
-
+    var weatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+    var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&cnt=5&units=standard&appid=${apiKey}`;
+   
     fetch(weatherUrl)
         .then(response => response.json())
         .then(data => {
@@ -25,20 +24,30 @@ function fetchWeatherData(city) {
 function displayWeatherData(data) {
     var currentCity = document.getElementById("currentCity");
     var temperatureInFahrenheit = kelvinToFahrenheit(data.main.temp);
-    currentCity.textContent = `Current Weather in ${data.name}: ${temperatureInFahrenheit}°F`;
+    var windSpeed = data.wind.speed;
+    var humidity = data.main.humidity;
+    var feelsLike = kelvinToFahrenheit(data.main.feels_like);
+    currentCity.innerHTML = `Current Weather in ${data.name}: ${temperatureInFahrenheit}°F<br>Wind Speed: ${windSpeed} m/s<br>Humidity: ${humidity} %<br>Feels Like: ${feelsLike}°F`;
 }
 
-function displayForecastData(forecastData) {
+function displayForecastData(forecastData, currentWeatherData) {
     var forecastSection = document.getElementById("forecast");
-    forecastSection.innerHTML = ""; // Clear previous forecast data
+    forecastSection.innerHTML = ""; 
 
     for (let i = 0; i < forecastData.list.length; i++) {
         var forecast = forecastData.list[i];
         var forecastItem = document.createElement("div");
-        forecastItem.textContent = `${forecast.dt_txt} - ${forecast.main.temp}°F`;
+        var temperature = forecast.main.temp;
+        var windSpeed = forecast.wind.speed;
+        var humidity = forecast.main.humidity;
+        var feelsLike = kelvinToFahrenheit(forecast.main.feels_like);
+
+        forecastItem.innerHTML = `${forecast.dt_txt} - ${temperature}°F<br>Wind Speed: ${windSpeed} m/s<br>Humidity: ${humidity} %<br>Feels Like: ${feelsLike}°F`;
         forecastSection.appendChild(forecastItem);
+        
     }
 }
+
 
 function kelvinToFahrenheit(kelvin) {
     return ((kelvin - 273.15) * 9/5 + 32).toFixed(2);
@@ -52,4 +61,5 @@ searchButton.addEventListener("click", () => {
 
 
 
+  
 
